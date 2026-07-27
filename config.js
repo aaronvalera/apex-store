922ca3be3760c2fde9348ddc68f7bd8b07aac2f8
Aaron Valera <aaronrvalera@gmail.com>
2026-07-27 13:30:54 -0400

    Configuration for production

---
+++ config.js
@@ -0,0 +1,7 @@
+const PAGE_URL = process.env.PAGE_URL;
+
+const MONGO_URI = process.env.NODE_ENV === "production"
+    ? process.env.MONGO_URI_PROD
+    : process.env.MONGO_URI_TEST;
+
+module.exports = { PAGE_URL, MONGO_URI };
