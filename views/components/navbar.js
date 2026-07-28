8e12089c6d27008e0f8fba4b0b2e2cc28bd72d02
Aaron Valera <aaronrvalera@gmail.com>
2026-07-27 20:24:38 -0400

    Upgrade navbar

--- views/components/navbar.js
+++ views/components/navbar.js
@@ -1,19 +1,45 @@
 const navbarContainer = document.querySelector(".navbar");
 
-const createNavbar = () => {
-    let navbar = ``;
-    navbar = `
+const createNavbar = (user = null) => {
+    let userMenuItems = ``;
+
+    if(!user) {
+        userMenuItems = `
+            <li><a href="/signup" class="font-semibold text-primary">Sign Up</a></li>
+            <li><a href="/login" class="font-semibold text-primary">Login</a></li>
+        `;
+    } else {
+        let adminOption = ``;
+        if(user.role === "admin") {
+            const isInAdminDashboard = window.location.pathname.includes("/admin");
+            if(isInAdminDashboard) {
+                adminOption = `
+                    <li><a href="/" class="font-semibold text-primary">Home</a></li>
+                `;
+            } else {
+                adminOption = `
+                    <li><a href="/admin/dashboard" class="font-semibold text-primary">Dashboard</a></li>
+                `;
+            }
+        }
+        userMenuItems = `
+            ${adminOption}
+            <li><a href="/settings" class="font-semibold text-primary">Settings</a></li>
+            <li><button id="logout-btn" class="text-red-500">Logout</button></li>   
+        `;
+    }
+    const navbar = `
         <div class="max-lg:collapse bg-base-200 lg:mb-48 shadow-sm w-full rounded-md">
             <input id="navbar-1-toggle" class="peer hidden" type="checkbox"/>
             <label for="navbar-1-toggle" class="fixed inset-0 hidden max-lg:peer-checked:block"></label>
             <div class="collapse-title navbar">
-                <div class="navbar-start">
+                <div class="navbar-start gap-2">
                     <label for="navbar-1-toggle" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"/>
+                        <svg id="mobile-menu-btn" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                         </svg>
                     </label>
-                    <a class="link-hover text-xl" href="/"><img src="/images/apex-logo.png" width="62"></a>
+                    <a class="link-hover text-xl" href="/"><img src="/images/apex-logo.png" width="55"></a>
                 </div>
                 <div class="navbar-center hidden lg:flex">
                     <ul class="menu menu-horizontal px-1">
@@ -61,9 +87,7 @@ const createNavbar = () => {
                             </div>
                         </div>
                         <ul tabindex="-1" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-42 p-2 shadow">
-                            <li><a href="/signup">Sign Up</a></li>
-                            <li><a href="/login">Login</a></li>
-                            <li><a href="#">Logout</a></li>
+                            ${userMenuItems}
                         </ul>
                     </div>
                 </div>
@@ -84,6 +108,30 @@ const createNavbar = () => {
         </div>
     `;
     navbarContainer.innerHTML = navbar;
-}
+
+    // MOBILE MENU BUTTON ICONS
+    const HAMBURGER_ICON = '<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>';
+    const CLOSE_ICON ='<path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>';
+
+    // CHANGE MOBILE MENU BUTTON ICON
+    const mobileMenuBtn = document.getElementById("mobile-menu-btn");
+    mobileMenuBtn.addEventListener("click", () => {
+        const isMenuOpened = mobileMenuBtn.classList.toggle("opened");
+        mobileMenuBtn.innerHTML = isMenuOpened ? CLOSE_ICON : HAMBURGER_ICON;
+    });
+
+    // LOGOUT
+    if(user) {
+        const logoutBtn = document.getElementById("logout-btn");
+        logoutBtn.addEventListener("click", async event => {
+            try {
+                await axios.post("/api/logout");
+                window.location.href = "/";
+            } catch (error) {
+                console.error("Error logging out", error);
+            }
+        });
+    }
+};
 
 createNavbar();
