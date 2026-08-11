706b358e26f9565b5f169a2a2395440428381b79
Aaron Valera <aaronrvalera@gmail.com>
2026-08-11 12:09:35 -0400

    Set checkout necessary routes

--- app.js
+++ app.js
@@ -15,8 +15,11 @@ const productsRouter = require("./controllers/products.js");
 const categoriesRouter = require("./controllers/categories.js");
 const productsAdminRouter = require("./controllers/productsAdmin.js");
 const categoriesAdminRouter = require("./controllers/categoriesAdmin.js");
+const paymentsRouter = require("./controllers/payments.js");
+const addressesRouter = require("./controllers/addresses.js");
+const ordersRouter = require("./controllers/orders.js");
 const { userExtractor, adminExtractor } = require("./middleware/auth.js");
-const { userPageGuard, adminPageGuard } = require("./middleware/pageGuards.js")
+const { userPageGuard, adminPageGuard } = require("./middleware/pageGuards.js");
 const app = express();
 
 (async () => {
@@ -39,10 +42,22 @@ app.use("/components", express.static(path.resolve("views", "components")));
 app.use("/helpers", express.static(path.resolve("views", "helpers")));  
 app.use("/", express.static(path.resolve("views", "home")));
 app.use("/signup", express.static(path.resolve("views", "signup")));
-app.use("/verify/:id/:token", express.static(path.resolve("views", "verify")));
 app.use("/signin", express.static(path.resolve("views", "signin")));
 app.use("/catalog", express.static(path.resolve("views", "catalog")));
 app.use("/product", express.static(path.resolve("views", "product")));
+app.use("/confirmation", express.static(path.resolve("views", "confirmation")));
+
+app.use("/verify", express.static(path.resolve("views", "verify")));
+app.get("/verify/:id/:token", (req, res) => {
+    res.sendFile(path.resolve("views", "verify", "index.html"));
+});
+
+// PROTECTED ROUTES
+app.get("/checkout", userPageGuard, (req, res) => {
+    res.sendFile(path.resolve("views", "checkout", "index.html"));
+});
+app.use("/checkout", express.static(path.resolve("views", "checkout"), { index: false }));
+
 app.get("/admin/dashboard", userPageGuard, adminPageGuard, (req, res) => {
     res.sendFile(path.resolve("views", "adminDashboard", "index.html"));
 });
@@ -57,6 +72,10 @@ app.use("/api/signin", signInRouter);
 app.use("/api/logout", logoutRouter);
 app.use("/api/products", productsRouter);
 app.use("/api/categories", categoriesRouter);
+app.use("/api/orders", userExtractor, ordersRouter);
+app.use("/api/payments", userExtractor, paymentsRouter);
+app.use("/api/addresses", userExtractor, addressesRouter);
+// Admin routes
 app.use("/api/admin/products", userExtractor, adminExtractor, productsAdminRouter);
 app.use("/api/admin/categories", userExtractor, adminExtractor, categoriesAdminRouter);
 
