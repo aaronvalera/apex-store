188d8a110de145bfa2ce425a3ad0a0d1cf29bd96
Aaron Valera <aaronrvalera@gmail.com>
2026-08-10 22:08:26 -0400

    Set new backend and frontend views

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const googleAuthRouter = require("./controllers/googleAuth.js");
 const usersRouter = require("./controllers/users.js");
 const signInRouter = require("./controllers/signin.js");
 const logoutRouter = require("./controllers/logout.js");
+const productsRouter = require("./controllers/products.js");
+const categoriesRouter = require("./controllers/categories.js");
+const productsAdminRouter = require("./controllers/productsAdmin.js");
+const categoriesAdminRouter = require("./controllers/categoriesAdmin.js");
+const { userExtractor, adminExtractor } = require("./middleware/auth.js");
+const { userPageGuard, adminPageGuard } = require("./middleware/pageGuards.js")
 const app = express();
 
 (async () => {
@@ -35,6 +41,12 @@ app.use("/", express.static(path.resolve("views", "home")));
 app.use("/signup", express.static(path.resolve("views", "signup")));
 app.use("/verify/:id/:token", express.static(path.resolve("views", "verify")));
 app.use("/signin", express.static(path.resolve("views", "signin")));
+app.use("/catalog", express.static(path.resolve("views", "catalog")));
+app.use("/product", express.static(path.resolve("views", "product")));
+app.get("/admin/dashboard", userPageGuard, adminPageGuard, (req, res) => {
+    res.sendFile(path.resolve("views", "adminDashboard", "index.html"));
+});
+app.use("/admin/dashboard", express.static(path.resolve("views", "adminDashboard"), { index: false }));
 
 app.use(morgan("tiny"));
 // BACKEND ROUTES
@@ -43,5 +55,9 @@ app.use("/api/auth", googleAuthRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/signin", signInRouter);
 app.use("/api/logout", logoutRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/categories", categoriesRouter);
+app.use("/api/admin/products", userExtractor, adminExtractor, productsAdminRouter);
+app.use("/api/admin/categories", userExtractor, adminExtractor, categoriesAdminRouter);
 
 module.exports = app;
